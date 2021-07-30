702e6ee265a8803ac3c6ed4fcc4db9dfb187242a
Robin Malhotra <5009041+codeOfRobin@users.noreply.github.com>
2021-07-30 01:03:44 +0000

    final fixes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 
-cat ~/.bashrc
 source ~/.bashrc
-
-asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git 
-asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git 
-asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git 
-KERL_CONFIGURE_OPTIONS=--without-javac KERL_BUILD_DOCS=yes asdf install erlang 24.0.4
-asdf install elixir 1.12.2-otp-24
-asdf install nodejs 14.15.1
-asdf global erlang 24.0.4
-asdf global elixir 1.12.2-otp-24
-asdf global nodejs 14.15.1
-asdf reshim erlang 
-asdf reshim elixir 
-asdf reshim nodejs
 
 mix local.hex --force \
   && mix local.rebar --force \
   && mix archive.install --force hex phx_new 1.5.8
 
-cd metricator && mix deps.get
+cd metricator/assets && npm install
+cd .. && mix deps.get
