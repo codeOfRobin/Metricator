6606263703812140c7e1cb2b50b3340c202f6846
Codespaces <codespaces@github.com>
2021-08-21 22:41:27 +0000

    Codespaces

--- metricator/lib/metricator_web/controllers/metric_controller.ex
+++ metricator/lib/metricator_web/controllers/metric_controller.ex
@@ -1,7 +1,13 @@
 defmodule MetricatorWeb.MetricController do
   use MetricatorWeb, :controller
 
+  alias Metricator.MetricData
+
   def create(conn, params) do
-    json(conn, params)
+    case MetricData.create_metric(params) do
+      {:ok, metric} ->
+        json(conn, params)
+      {:error }
+    end
   end
 end
--- metricator/test/metricator_web/features/send_metric_request_successfully_returns_and_saves_test.exs
+++ metricator/test/metricator_web/features/send_metric_request_successfully_returns_and_saves_test.exs
@@ -1,16 +1,25 @@
 defmodule MetricatorWeb.SendMetricSuccesfullyTest do
   use MetricatorWeb.ConnCase, async: true
 
+  alias Metricator.MetricData
+
   test "user sends dictionary to API and it is recorded successfully", %{conn: conn} do
-    currentTime = DateTime.utc_now() |> DateTime.to_iso8601()
+    currentTime = DateTime.utc_now() |> DateTime.to_unix(:microsecond)
+
     metric_data = %{
       "properties" => %{"foo" => "bar"},
       "timestamp" => currentTime
     }
+
     conn = post(build_conn(), "/record", metric_data)
+
     assert json_response(conn, 200) == %{
-      "properties" => %{"foo" => "bar"},
-      "timestamp" => currentTime
-    }
+             "properties" => %{"foo" => "bar"},
+             "timestamp" => currentTime
+           }
+
+    [metric | _] = MetricData.all_metrics()
+    assert metric.properties == metric_data["properties"]
+    assert metric.timestamp == metric_data["timestamp"]
   end
 end
