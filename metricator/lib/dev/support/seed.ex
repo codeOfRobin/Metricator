1a0dba7b0dd66b01a6bec7154424ee15d71b9867
Robin Malhotra <5009041+codeOfRobin@users.noreply.github.com>
2021-09-01 01:05:15 +0000

    WIP

--- metricator/lib/dev/support/seed.ex
+++ metricator/lib/dev/support/seed.ex
@@ -3,18 +3,22 @@ defmodule Metricator.Seed do
   alias Metricator.Repo
 
 
-  def generate_timestamp_in_current_month(index) do
-    current_day =
-    %DateTime{
-      %Date{
+  def generate_timestamp_in_current_month() do
+    current_day = DateTime.utc_now()
 
+    timestamps = Enum.map(1..current_day.day, fn index ->
+      DateTime.new{
+        Date.new!(current_day.year, current_day.month, index),
+        Time.new!(:rand.uniform(12), :rand.uniform(60), :rand.uniform(60))
       }
-    }
+    end)
+
+
   end
 
   defp user_activity_event_properties() do
     %{
-      "userID" => Ecto.UUID.generate()
+      "userID" => Ecto.UUID.generate(),
       "activity" => "launch"
     }
   end
