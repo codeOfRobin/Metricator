63115e50d924c2c656fde08d3f0373097ac1fc3e
Robin Malhotra <5009041+codeOfRobin@users.noreply.github.com>
2021-09-10 00:06:47 +0000

    Adds methods to save events

--- metricator/lib/dev/support/date_generator.ex
+++ metricator/lib/dev/support/date_generator.ex
@@ -1,14 +1,14 @@
 defmodule Metricator.DateGenerator do
 
   def generate_random_timestamps_for_days_ago(days, from_date \\ DateTime.utc_now(), events_per_day_upper_bound \\ 30) do
-    Enum.map(0..(days - 1), fn index ->
+    Enum.flat_map(0..(days - 1), fn index ->
       older_date = Timex.shift(from_date, days: -index)
 
       number_of_timestamps = :rand.uniform(events_per_day_upper_bound)
       Enum.map(0..number_of_timestamps, fn _index ->
-        DateTime.new(
+        DateTime.new!(
           older_date |> DateTime.to_date(),
-          Time.new!(:rand.uniform(23), :rand.uniform(59), :rand.uniform(59))
+          Time.new!(:rand.uniform(23), :rand.uniform(59), :rand.uniform(59), :rand.uniform(1000000))
         )
       end)
     end)
